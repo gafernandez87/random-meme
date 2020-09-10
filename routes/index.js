34f588010d6ce9bc76ea7af2f6b1fb01427ad9f3
Gaston Fernandez <gaston.fernandez@softvision.com>
2020-09-10 19:49:33 -0300

    added post command

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,18 @@ router.get('/meme', async (_, res) => {
   }
 });
 
+router.post('/meme', async (_, res) => {
+  try {
+    const randomMeme = await axios('https://imgflip.com/ajax_img_flip?current_iid=100923471');
+    const memePage = await axios(`https://imgflip.com${randomMeme.data}`);
+    const $ = cheerio.load(memePage.data);
+    const src = $('#im').attr('src');
+    const url = `https://${src.substring(2, src.length)}`;
+    res.status(200).send(url);
+  } catch (err) {
+    console.log(err);
+    res.status(200).send('https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png');
+  }
+});
+
 module.exports = router;
