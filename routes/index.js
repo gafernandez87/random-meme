e0911ca39d0a5f14025c38ab8855778951c3e3ef
Gaston Fernandez <gaston.fernandez@softvision.com>
2020-09-11 12:44:22 -0300

    send meme to slack

--- routes/index.js
+++ routes/index.js
@@ -9,31 +9,53 @@ router.get('/', (_, res) => {
 });
 
 router.get('/meme', async (_, res) => {
+  let url = '';
   try {
     const randomMeme = await axios('https://imgflip.com/ajax_img_flip?current_iid=100923471');
     const memePage = await axios(`https://imgflip.com${randomMeme.data}`);
     const $ = cheerio.load(memePage.data);
     const src = $('#im').attr('src');
-    const url = `https://${src.substring(2, src.length)}`;
+    url = `https://${src.substring(2, src.length)}`;
+
     res.status(200).send(url);
   } catch (err) {
     console.log(err);
-    res.status(200).send('https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png');
+    url = 'https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png';
+    res.status(200).send('error');
   }
+
+  axios.get('https://slack.com/api/chat.postMessage', {
+    params: {
+      token: 'xoxb-68729767794-1368981853265-10eelhZs7XzUZPVc6M2vFj3p',
+      channel: 'memes',
+      text: url,
+    },
+  }).catch(console.log);
 });
 
 router.post('/meme', async (_, res) => {
+  let url = '';
   try {
     const randomMeme = await axios('https://imgflip.com/ajax_img_flip?current_iid=100923471');
     const memePage = await axios(`https://imgflip.com${randomMeme.data}`);
     const $ = cheerio.load(memePage.data);
     const src = $('#im').attr('src');
-    const url = `https://${src.substring(2, src.length)}`;
+    url = `https://${src.substring(2, src.length)}`;
+
     res.status(200).send(url);
   } catch (err) {
     console.log(err);
-    res.status(200).send('https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png');
+    url = 'https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png';
+    res.status(200).send('error');
   }
+
+  axios.get('https://slack.com/api/chat.postMessage', {
+    params: {
+      token: 'xoxb-68729767794-1368981853265-10eelhZs7XzUZPVc6M2vFj3p',
+      channel: 'memes',
+      text: url,
+    },
+  }).catch(console.log);
 });
 
 module.exports = router;
