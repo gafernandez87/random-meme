4c72b2bdc180168856c907a9cef9d8ce75e15187
Gaston Fernandez <gaston.fernandez@softvision.com>
2020-09-11 16:20:58 -0300

    elegant ode

--- routes/index.js
+++ routes/index.js
@@ -4,58 +4,52 @@ const cheerio = require('cheerio');
 
 const router = express.Router();
 
+
+const { token, channel } = process.env;
+
+const getRandomMemeUrl = async () => {
+  try {
+    const randomMeme = await axios('https://imgflip.com/ajax_img_flip?current_iid=100923471');
+    const memePage = await axios(`https://imgflip.com${randomMeme.data}`);
+    const $ = cheerio.load(memePage.data);
+    const src = $('#im').attr('src');
+    return `https://${src.substring(2, src.length)}`;
+  } catch (err) {
+    console.log(err);
+    return 'https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png';
+  }
+};
+
 router.get('/', (_, res) => {
   res.status(200).send({ response: 'I am alive' });
 });
 
 router.get('/meme', async (_, res) => {
-  let url = '';
-  try {
-    const randomMeme = await axios('https://imgflip.com/ajax_img_flip?current_iid=100923471');
-    const memePage = await axios(`https://imgflip.com${randomMeme.data}`);
-    const $ = cheerio.load(memePage.data);
-    const src = $('#im').attr('src');
-    url = `https://${src.substring(2, src.length)}`;
-
-    res.status(200).send(url);
-  } catch (err) {
-    console.log(err);
-    url = 'https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png';
-    res.status(200).send('error');
-  }
+  const url = await getRandomMemeUrl();
 
   axios.get('https://slack.com/api/chat.postMessage', {
     params: {
-      token: 'xoxb-68729767794-1368981853265-10eelhZs7XzUZPVc6M2vFj3p',
-      channel: 'memes',
+      token,
+      channel,
       text: url,
     },
   }).catch(console.log);
+
+  res.status(200).send(url);
 });
 
 router.post('/meme', async (_, res) => {
-  let url = '';
-  try {
-    const randomMeme = await axios('https://imgflip.com/ajax_img_flip?current_iid=100923471');
-    const memePage = await axios(`https://imgflip.com${randomMeme.data}`);
-    const $ = cheerio.load(memePage.data);
-    const src = $('#im').attr('src');
-    url = `https://${src.substring(2, src.length)}`;
-
-    res.status(200).send(url);
-  } catch (err) {
-    console.log(err);
-    url = 'https://sayingimages.com/wp-content/uploads/try-again-fail-meme.png';
-    res.status(200).send('error');
-  }
+  const url = await getRandomMemeUrl();
 
   axios.get('https://slack.com/api/chat.postMessage', {
     params: {
-      token: 'xoxb-68729767794-1368981853265-10eelhZs7XzUZPVc6M2vFj3p',
-      channel: 'memes',
+      token,
+      channel,
       text: url,
     },
   }).catch(console.log);
+
+  res.status(200).send(url);
 });
 
 module.exports = router;
